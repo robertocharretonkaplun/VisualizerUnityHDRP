fd30e7a20ddbe8d44d63909b5d1d843ab5d20767
CerberusX99 <114266496+CerberusX99@users.noreply.github.com>
2024-07-06 15:07:44 -0600

    Prototipo 1.0
    -Cambios en la UI
    -Cambios en QOA
    -Nuevos elementos de construcción +

--- Visualizer/Assets/Scripts/ItemController.cs
+++ Visualizer/Assets/Scripts/ItemController.cs
@@ -5,28 +5,26 @@ using TMPro;
 public class ItemController : MonoBehaviour
 {
     public int itemID; // ID del item 
-    public int itemAmount; // Cantidad de items
+    
     public TextMeshProUGUI itemAmountText; // Texto de la cantidad de items
     public bool Clicked = false; // Si el item fue clickeado
     private LevelEditor_Manager levelEditor_Manager; // Manager del editor de niveles
     void Start()
     {
         // Referecia al texto de la cantidad de items y al manager del editor de niveles
-        itemAmountText.text = itemAmount.ToString();
+       
         levelEditor_Manager = GameObject.FindGameObjectWithTag("LevelEditor_Manager").GetComponent<LevelEditor_Manager>();
     }
     public void ButtonClick()
     {
         // Si la cantidad de items es mayor a 0, se activa el click
-        if(itemAmount > 0)
-        {
+        
         Vector3 screenPosition=new Vector3(Input.mousePosition.x,Input.mousePosition.y, Input.mousePosition.z);
         Vector3 worldPosition=Camera.main.ScreenToWorldPoint(screenPosition);    
         Clicked = true;
-        itemAmount--;
-        itemAmountText.text = itemAmount.ToString();
+        
         levelEditor_Manager.CurrentButtonPressed = itemID;
-        }
+        
     }
     
 }
