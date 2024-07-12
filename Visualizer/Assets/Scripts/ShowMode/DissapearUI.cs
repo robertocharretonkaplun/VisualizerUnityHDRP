d709187f9c4a35be8391a651a55cef43165aace7
Simiobanana <114266340+Simiobanana@users.noreply.github.com>
2024-07-12 15:45:30 -0600

    Update show mode scene
    actualice el show mode donde elimine 2 botones, ya con escape puede ocultar o mostrar la UI, Al pulsar play comienza a correr el tiempo y al pulsar pausa se detiene (esto en un futuro se cambiara a que se reproduzca el recorrido o se detenga)

--- Visualizer/Assets/Scripts/ShowMode/DissapearUI.cs
+++ Visualizer/Assets/Scripts/ShowMode/DissapearUI.cs
@@ -5,39 +5,75 @@ using UnityEngine.UI;
 
 public class DissapearUI : MonoBehaviour
 {
-    public Button[] ButtonsToOff;
-    public Button[] ButtonsToOn;
-    public Button ButtonInjteractUIOn;
-    public Button ButtonInjteractUIOff;
+    public Button[] Buttons;
+    [SerializeField] private bool ButtonsActive;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 1f;
+        ButtonsActive = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        UiOff_On(); 
+    }
+
+    private void FixedUpdate()
+    {
+        if(Time.timeScale > 0f) 
+        {
+            Debug.Log("Animacion");
+        }
+        else if(Time.timeScale < 1f) 
+        {
+            Debug.Log("Pausa");
+        }
+        
+    }
+
+    public void Play()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void UiOff_On()
+    {
+        if (ButtonsActive == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                foreach (Button button in Buttons)
+                {
+                    button.gameObject.SetActive(false);
+                }
+                ButtonsActive = false;
+            }
+            
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                foreach (Button button in Buttons)
+                {
+                    button.gameObject.SetActive(true);
+                }
+                ButtonsActive = true;
+            }
+            
+        }
+        
+    }
+
+    public void StopButton()
+    {
        
-    }
-
-    public void UIOff()
-    {
-        foreach (Button button in ButtonsToOff)
-        {
-            button.gameObject.SetActive(false);
-        }
-        ButtonInjteractUIOn.gameObject.SetActive(false);
-        ButtonInjteractUIOff.gameObject.SetActive(true);
-    }
-
-    public void UIOn()
-    {
-        foreach (Button button in ButtonsToOn)
-        {
-            button.gameObject.SetActive(true);
-        }
-        ButtonInjteractUIOn.gameObject.SetActive(true);
-        ButtonInjteractUIOff.gameObject.SetActive(false);
     }
 }
