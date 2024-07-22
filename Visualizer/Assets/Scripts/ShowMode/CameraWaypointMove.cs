8ab085c36c883eed087f6b363229ea332aab96b1
Simiobanana <114266340+Simiobanana@users.noreply.github.com>
2024-07-22 13:54:58 -0600

    Merge pull request #8 from robertocharretonkaplun/ShowTime
    Show time

---
+++ Visualizer/Assets/Scripts/ShowMode/CameraWaypointMove.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraWaypointMove : MonoBehaviour
+{
+    /// <summary>
+    /// Initialize variables and list to control how and when start camera movement throught the waypoints
+    /// </summary>
+    
+    [Header("List of waytpoints to use and variable to show which has traveled")]
+    public List<Transform> waypoints = new List<Transform>();
+    [SerializeField]private int currentWaypointIndex = 0;
+
+    [Header("Variables to move camera")]
+    public float moveSpeed = 5f; 
+    public float waitTime = 1f; 
+
+    [Header("Booleans to debug and when the camera should start moving")]
+    [SerializeField]private bool isMoving = false;
+    [SerializeField]private bool shouldMove = false;
+
+    /// <summary>
+    /// Establish a if condition with the booleans and list of waypoints to start coroutine to move
+    /// </summary>
+    void Update()
+    {
+        if (shouldMove && !isMoving && waypoints.Count > 0)
+        {
+            StartCoroutine(MoveToWaypoint());
+        }
+    }
+
+    /// <summary>
+    /// Create the IEnumerator to contain logic to move camera
+    /// </summary>
+    IEnumerator MoveToWaypoint()
+    {
+        //Establish boolean on true to control when start to move
+        isMoving = true;
+
+        //Get transform component to waypoint from the index
+        Transform targetWaypoint = waypoints[currentWaypointIndex];
+
+        //Get actual transform position how start position
+        Vector3 startPosition = transform.position;
+
+        //Get transform position from the targetwaypoint how end position
+        Vector3 endPosition = targetWaypoint.position;
+
+        //Calculate distance beetwen start position and end position
+        float distance = Vector3.Distance(startPosition, endPosition);
+
+        //Save time when IEnumerator start
+        float startTime = Time.time;
+
+        //This while is to achieve fluid movement when transferring between waypoints
+        while (Vector3.Distance(transform.position, endPosition) > 0.1f)
+        {
+            //Calculate time transcurrent beetwen start time and actual time
+            float elapsedTime = Time.time - startTime;
+            //The movement is because a generate linear interpolation to get a slow and fluid movement
+            transform.position = Vector3.Lerp(startPosition, endPosition, elapsedTime * moveSpeed / distance);
+            yield return null;
+        }
+
+        //Update end position value
+        transform.position = endPosition;
+
+        //Wait seconds to travel a new waypoint
+        yield return new WaitForSeconds(waitTime);
+
+        //Update count to index from waypoints
+        currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Count;
+
+        //Establish boolean on true to control when stop to move
+        isMoving = false;
+    }
+
+    /// <summary>
+    /// Void to add waypoints to list of waypoints
+    /// </summary>
+
+    public void AddWaypoint(Transform waypoint)
+    {
+        waypoints.Add(waypoint);
+    }
+
+    /// <summary>
+    /// Void to control the start move
+    /// </summary>
+    public void StartMoving()
+    {
+        shouldMove = true;
+    }
+}
---
+++ Visualizer/Assets/Scripts/ShowMode/ShowModeController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is control the play/pause/stop button
+/// </summary>
+public class ShowModeController : MonoBehaviour
+{
+    /// <summary>
+    /// I need a ref to CameraWaypoint script to initialize the movement and stop them.
+    /// </summary>
+
+    [Header("Script reference")]
+    public CameraWaypointMove CameraWaypointMoveRef; //Get camera waypoint move script
+
+
+    /// <summary>
+    /// initialize the timescale to one to avoid bugs
+    /// </summary>
+    void Start()
+    {
+        Time.timeScale = 1f; //Set start timescale normal
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Only need fixed update to debug timescale
+    /// </summary>
+    private void FixedUpdate()
+    {
+        if (Time.timeScale > 0f)
+        {
+            Debug.Log("Animacion");
+        }
+        else if (Time.timeScale < 1f)
+        {
+            Debug.Log("Pausa");
+        }
+
+    }
+
+    /// <summary>
+    /// This void will be linked to the play button (In the future this function realize the animation with the waypoints)
+    /// </summary>
+    public void Play()
+    {
+        Time.timeScale = 1.0f; //Set timescale normal
+        CameraWaypointMoveRef.StartMoving(); //Get function to starting move from CameraWaypointMoveRef
+    }
+
+    /// <summary>
+    /// This void will be linked to the pause button (In the future this function pause the animation with the waypoints)
+    /// </summary>
+    public void Pause()
+    {
+        Time.timeScale = 0f; //Set timescale paused
+    }
+
+    /// <summary>
+    /// This void will be linked to the stop button (In the future this function stop the animation with the waypoints)
+    /// </summary>
+    public void StopButton()
+    {
+
+    }
+}
