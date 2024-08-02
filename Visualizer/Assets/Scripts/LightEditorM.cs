57cd7caff939f9adc1308a257bd46dd6bb91ac60
CerberusX99 <114266496+CerberusX99@users.noreply.github.com>
2024-08-02 16:16:55 -0600

    UI QOA & Camera
    -Cambios en la interfaz de medidas y de colores, iconos y tipo de botones
    -Función de teletransportar cámara a un objeto presionando F
    -Investigación de funcionalidades de RuntimeHierarchy

--- Visualizer/Assets/Scripts/LightEditorM.cs
+++ Visualizer/Assets/Scripts/LightEditorM.cs
@@ -26,7 +26,7 @@ public class LightEditorM : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
+            //DontDestroyOnLoad(gameObject);
         }
         else
         {
