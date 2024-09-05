7a2d8e41585ac4e9b94d1fec0e9b8ad6bbeba42c
Alejandro Urvieta Gonzalez <114266340+Simiobanana@users.noreply.github.com>
2024-09-05 12:08:42 -0600

    4 new unit tests

---
+++ Visualizer/Assets/Tests/PlayMode/Animation_Drawer_Test.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+//This methos is to check play animation when "AccesBar_ContentBtn" is clicked
+public class Animation_Drawer_Test
+{
+    //References to script, canvas, button and drawer
+    private GameObject canvas;
+    private GameObject accessBarContentBtn;
+    private GameObject drawer;
+    private AnimationC animationCScript;
+
+    [UnitySetUp]
+    public IEnumerator SetUp()
+    {
+        //Charge "Alpha" scene
+        SceneManager.LoadScene("Alpha");
+
+        //Wait to scene is charge
+        yield return new WaitForSeconds(1); 
+        Debug.Log("Escena cargada");
+
+        //Find canvas object and assign 
+        canvas = GameObject.Find("Canvas");
+        Assert.IsNotNull(canvas, "No se encontró el Canvas en la escena.");
+        Debug.Log("Canvas encontrado");
+
+        //Find button object and assign
+        accessBarContentBtn = GameObject.Find("AcccessBar_ContentBtn");
+        Assert.IsNotNull(accessBarContentBtn, "No se encontró el botón 'AccessBar_ContentBtn' en la escena.");
+        Debug.Log("Boton encontrado");
+
+        //Find Drawer object and assign
+        drawer = GameObject.Find("Drawer");
+        Assert.IsNotNull(drawer, "No se encontró el objeto 'Drawer' en la escena.");
+        Debug.Log("Drawer encontrado");
+
+        //Find Script and assign
+        animationCScript = drawer.GetComponent<AnimationC>();
+        Assert.IsNotNull(animationCScript, "No se encontró el script 'AnimationC' en el objeto 'Drawer'.");
+        Debug.Log("Script encontrado");
+    }
+
+    [UnityTest]
+    public IEnumerator TestAnimationIsPlaying()
+    {
+        //Simulate click on button to drawer is down
+        Button button = accessBarContentBtn.GetComponent<Button>();
+        button.onClick.Invoke();
+        yield return new WaitForSeconds(0.1f);
+
+        //Check the animation is down
+        Assert.IsTrue(animationCScript.isDown, "La animación se ha reproducido hacia abajo como se esperaba.");
+        Assert.IsFalse(animationCScript.isUp, "La animación no ha cambiado el estado a 'isUp = false' como se esperaba.");
+
+        //Wait seconds to check end animation
+        yield return new WaitForSeconds(1f);
+
+        //Simulate click on button to drawer is up
+        button.onClick.Invoke();
+        yield return new WaitForSeconds(0.1f);
+
+        //Check the animation is up
+        Assert.IsTrue(animationCScript.isUp, "La animación se ha reproducido hacia arriba como se esperaba.");
+        Assert.IsFalse(animationCScript.isDown, "La animación no ha cambiado el estado a 'isDown = false' como se esperaba.");
+
+        yield return new WaitForSeconds(1f);
+    }
+}
---
+++ Visualizer/Assets/Tests/PlayMode/UI_Show_Mode_Test.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+//Method to check funtion to UI show mode button
+public class UI_Show_Mode_Test
+{
+    private GameObject showModeCanvas;
+    private GameObject Waypointmanager;
+    private GameObject DissapearUI;
+    private Button playButton;
+    private Button pauseButton;
+    private Button stopButton;
+    private Button waypointsButton;
+    private DissapearUI disappearUIScript;
+    private ShowModeController showModeControllerScript;
+    private Waypoints waypointsScript;
+
+    [UnitySetUp]
+    public IEnumerator SetUp()
+    {
+        //Charge scene "AlphaShowMode"
+        SceneManager.LoadScene("AlphaShowMode");
+        yield return new WaitForSeconds(1); 
+
+        //Find GameObjects
+        showModeCanvas = GameObject.Find("ShowModeCanvas");
+        Assert.IsNotNull(showModeCanvas, "No se encontró el ShowModeCanvas en la escena.");
+        Waypointmanager = GameObject.Find("WaypointsManager");
+        Assert.IsNotNull(Waypointmanager, "No se encontró el ShowModeCanvas en la escena.");
+        DissapearUI = GameObject.Find("DissapearUI");
+        Debug.Log("Se encontro el Canvas, DissapearUI y WaypointsManager");
+
+        //Find buttons
+        playButton = GameObject.Find("PlayButton").GetComponent<Button>();
+        pauseButton = GameObject.Find("PauseButton").GetComponent<Button>();
+        stopButton = GameObject.Find("StopButton").GetComponent<Button>();
+        waypointsButton = GameObject.Find("WaypointsButton").GetComponent<Button>();
+       
+
+        Assert.IsNotNull(playButton, "No se encontró el botón 'PlayButton' en la escena");
+        Assert.IsNotNull(pauseButton, "No se encontró el botón 'PauseButton' en la escena");
+        Assert.IsNotNull(stopButton, "No se encontró el botón 'StopButton' en la escena");
+        Assert.IsNotNull(waypointsButton, "No se encontró el botón 'WaypointsButton' en la escena");
+        Debug.Log("Se encontraron los botones");
+
+        //Find scripts
+        disappearUIScript = DissapearUI.GetComponent<DissapearUI>();
+        Debug.Log("Se encontro DissapearUI");
+        showModeControllerScript = showModeCanvas.GetComponent<ShowModeController>();
+        Debug.Log("Se encontro ShowModeController");
+        waypointsScript = Waypointmanager.GetComponent<Waypoints>();
+        Debug.Log("Se encontro Waypoints");
+
+        Assert.IsNotNull(disappearUIScript, "No se encontró el script 'DissapearUI' en el objeto 'ShowModeCanvas'");
+        Assert.IsNotNull(showModeControllerScript, "No se encontró el script 'ShowModeController' en el objeto 'ShowModeCanvas'");
+        Assert.IsNotNull(waypointsScript, "No se encontró el script 'Waypoints' en el objeto 'WaypointsManager'");
+        Debug.Log("Se encontraron los scripts");
+    }
+
+    [UnityTest]
+    public IEnumerator TestButtonFunctionality()
+    {
+        //Simulate play
+        playButton.onClick.Invoke();
+        yield return new WaitForSeconds(0.1f);
+        Assert.AreEqual(1f, Time.timeScale, "La aplicacion no está en modo de reproducción como se esperaba");
+        Debug.Log("Aplicacion en play");
+
+        //Simulate stop
+        stopButton.onClick.Invoke();
+        yield return new WaitForSeconds(0.1f);
+        Debug.Log("Aplicacion en stop");
+
+        //Simulate play again
+        playButton.onClick.Invoke();
+        yield return new WaitForSeconds(0.1f);
+        Assert.AreEqual(1f, Time.timeScale, "La aplicacion no se ha reanudado correctamente después del stop");
+        Debug.Log("Aplicacion en play");
+
+        //Activate creation waypoints mode
+        waypointsButton.onClick.Invoke();
+        yield return new WaitForSeconds(0.1f);
+        Debug.Log("Se activo el modo creacion de waypoints");
+
+        //Deactivate creation waypoints mode
+        waypointsButton.onClick.Invoke();
+        yield return new WaitForSeconds(0.1f);
+        Debug.Log("Se desactivo el modo creacion de waypoints");
+
+        //Simulate pause
+        pauseButton.onClick.Invoke();
+        yield return new WaitForSeconds(0.1f);
+        Assert.AreEqual(0f, Time.timeScale, "La aplicacion no está en pausa como se esperaba");
+        Debug.Log("Aplicacion en pausa");
+
+        //Dissapear UI
+        yield return new WaitForSeconds(0.1f);
+        Debug.Log("Wait");
+        Assert.IsTrue(disappearUIScript.gameObject.activeSelf, "La UI debería estar activa antes de intentar desactivarla");
+        Debug.Log("Esta activo el script");
+        disappearUIScript.UiOff_On();
+        Debug.Log("Se desactivo la UI");
+        yield return new WaitForSeconds(1.0f);
+        Assert.IsFalse(disappearUIScript.gameObject.activeSelf, "La UI no se desactivó al llamar a la funcion");
+
+    }
+}
