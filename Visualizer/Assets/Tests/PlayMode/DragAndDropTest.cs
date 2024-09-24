ed033c9e202088fa4308e7ec5294d58c716f6774
Alejandro Urvieta Gonzalez <114266340+Simiobanana@users.noreply.github.com>
2024-09-24 11:56:13 -0600

    Drag and drop test complete

---
+++ Visualizer/Assets/Tests/PlayMode/DragAndDropTest.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class DragAndDropTest
+{
+    private GameObject table;
+    private Renderer[] childRenderers;
+    private Material[] originalMaterials;
+
+    [UnitySetUp]
+    public IEnumerator SetUp()
+    {
+        //Load scene "D&D"
+        SceneManager.LoadScene("D&D");
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Escena cargada");
+
+        //Look table
+        table = GameObject.Find("Table");
+        Assert.IsNotNull(table, "El objeto 'Table' no fue encontrado en la escena");
+        Debug.Log("Objeto encontrado");
+
+        //Get renderer components
+        childRenderers = table.GetComponentsInChildren<Renderer>();
+        Assert.IsTrue(childRenderers.Length > 0, "No se encontraron Renderers en los hijos del objeto 'Table'");
+        Debug.Log("Renderers de hijos obtenidos");
+
+        //Save original materials to compare with new materials
+        originalMaterials = new Material[childRenderers.Length];
+        for (int i = 0; i < childRenderers.Length; i++)
+        {
+            originalMaterials[i] = childRenderers[i].material;
+        }
+        Debug.Log("Materiales originales guardados");
+    }
+
+    [UnityTest]
+    public IEnumerator DragAndDropMaterialToGameObject()
+    {
+        //Wait 5 seconds to change material
+        Debug.Log("Esperando 5 segundos para que puedas cambiar manualmente el material");
+        yield return new WaitForSeconds(5f);
+
+        //Check if material is change into children renderer objetcts
+        for (int i = 0; i < childRenderers.Length; i++)
+        {
+            Renderer renderer = childRenderers[i];
+            Material originalMaterial = originalMaterials[i];
+
+            //Check to material has changed
+            Assert.AreNotEqual(originalMaterial, renderer.material, "El material no cambió en uno de los hijos de 'Table'");
+            Debug.Log("Material cambiado correctamente en el Renderer de un hijo");
+        }
+    }
+}
