aa247673e81e0df6a84f35698be101ab8f15f3e7
Alejandro Urvieta Gonzalez <114266340+Simiobanana@users.noreply.github.com>
2024-10-08 12:12:37 -0600

    Navigation on first person ready

--- Visualizer/Assets/Scripts/ShowMode/NavMeshNavigation.cs
+++ Visualizer/Assets/Scripts/ShowMode/NavMeshNavigation.cs
@@ -6,22 +6,56 @@ using UnityEngine.AI;
 //Class to navigate in runtime
 public class NavMeshNavigation : MonoBehaviour
 {
-
-    public Camera cam;
+    //Reference to my main camera
+    public Camera CameraDefault;
+    //Reference to my camera inside capsule/player
+    public Camera CameraFirstPerson;
+    //Reference to Navmesh
     public UnityEngine.AI.NavMeshAgent agent;
 
     //Bool to manage change mode
-    public bool NavigationMode;
+    public bool NavigationMode = true;
 
     void Start()
     {
-       agent = GetComponent<NavMeshAgent>();
-       cam = GetComponent<Camera>();
-       NavigationMode = false;
+        //Get my Navmesh component
+        agent = GetComponent<NavMeshAgent>();
+        //At start my First Person camera is off
+        CameraFirstPerson.enabled = false;
     }
 
     void Update()
     {
-        
+        //If I push F key
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            //I`ll change my camera 
+            ChangeNavigationMode();
+        }
+
+        //If I push my left click mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Use active camera to make a Raycast to set agent destination
+            Camera activeCamera = NavigationMode ? CameraFirstPerson : CameraDefault;
+            Ray ray = activeCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+
+    }
+
+    //Method to change between my main camera or my First Person camera
+    void ChangeNavigationMode()
+    {
+        NavigationMode = !NavigationMode;
+
+        //Enable and disable my cameras
+        CameraDefault.enabled = !NavigationMode;
+        CameraFirstPerson.enabled = NavigationMode;
     }
 }
